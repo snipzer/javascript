63e7565f1239c81f27855cf0849fc2b125219021
snipzer <l.arif@laposte.net>
2017-03-16 10:30:51 +0100

    Ajout de création d'objet et console.log

--- js/base.js
+++ js/base.js
@@ -1,16 +1,40 @@
-var toto;
 
-// Récupère une entrée utilisateur
-toto = prompt("Entré votre nom:");
+// BASE -----------------------------------------------
+// var toto;
+//
+// // Récupère une entrée utilisateur
+// toto = prompt("Entré votre nom:");
+//
+// // Pose une question et renvois un booléen
+// var confirm = confirm("Question");
+//
+// if(confirm)
+// {
+//     document.write(toto);
+// }
+// else
+// {
+//     document.write("Pas confirmer");
+// }
 
-// Pose une question et renvois un booléen
-var confirm = confirm("Question");
 
-if(confirm)
-{
-    document.write(toto);
-}
-else
-{
-    document.write("Pas confirmer");
-}
+// Création d'un objet
+var obj = {
+    prop: "value",
+    prop2: "value2"
+};
+
+// Appel de la propriétée
+document.write(obj.prop2);
+
+// Rajout d'une propriétée dans l'objet
+obj['prop3'] = 4;
+
+document.write(obj.prop3);
+
+// Ecrit dans la console
+console.log(obj);
+// Passe une warning dans la console
+console.warn(obj);
+// Passe une erreur dans la console
+console.error(obj);
