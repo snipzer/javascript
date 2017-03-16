c047fec01b25e7a064a06dd646ae00140c96c3e7
snipzer <l.arif@laposte.net>
2017-03-16 10:02:23 +0100

    Ajout du confirm

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 var toto;
 
+// Récupère une entrée utilisateur
 toto = prompt("Entré votre nom:");
 
-document.write(toto);
+// Pose une question et renvois un booléen
+var confirm = confirm("Question");
+
+if(confirm)
+{
+    document.write(toto);
+}
+else
+{
+    document.write("Pas confirmer");
+}
