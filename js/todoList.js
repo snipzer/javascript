aa6cdebc01b0530314098b3a0e65d238fc50cf86
snipzer <l.arif@laposte.net>
2017-03-20 13:35:28 +0100

    Ajout des boutons UP et DOWN, les li sont maintenants supprimer au lieux d'être hidden en cliquant sur DELETE

--- js/todoList.js
+++ js/todoList.js
@@ -48,7 +48,63 @@ function newElement()
         close[i].onclick = function ()
         {
             var div = this.parentElement;
-            div.style.display = "none";
+            document.querySelector("#myUL").removeChild(div);
+        }
+    }
+
+
+    // Create a DOWN button and append it to each list item
+    var spanDown = document.createElement("span");
+    var txtDown = document.createTextNode("DOWN");
+
+    spanDown.className = "down";
+    spanDown.appendChild(txtDown);
+    li.appendChild(spanDown);
+
+    // Click on a up button to up the current list item
+    var Down = document.querySelectorAll(".down");
+
+    for(i = 0; i < Down.length; i++)
+    {
+        Down[i].onclick = function ()
+        {
+            /*
+            var liNext = this.parentElement.nextElementSibling.nextElementSibling;
+            var thisLi = this.parentElement;
+            document.querySelector("#myUL").insertBefore(thisLi, liNext);
+            */
+            var liNext = this.parentElement.nextElementSibling;
+            if(liNext === null)
+            {
+                liNext = document.querySelector("#myUL").firstElementChild;
+                document.querySelector("#myUL").insertBefore(this.parentElement, liNext);
+            }
+            else
+            {
+                document.querySelector("#myUL").insertBefore(liNext, this.parentElement);
+            }
+
+        }
+    }
+
+    // Create a UP button and append it to each list item
+    var spanUp = document.createElement("span");
+    var txtUp = document.createTextNode("UP");
+
+    spanUp.className = "up";
+    spanUp.appendChild(txtUp);
+    li.appendChild(spanUp);
+
+    // Click on a up button to up the current list item
+    var Up = document.querySelectorAll(".up");
+
+    for(i = 0; i < Up.length; i++)
+    {
+        Up[i].onclick = function ()
+        {
+            var liBefore = this.parentElement.previousElementSibling;
+            var thisLi = this.parentElement;
+            document.querySelector("#myUL").insertBefore(thisLi, liBefore);
         }
     }
 }
