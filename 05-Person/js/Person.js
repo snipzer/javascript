6e40547a181e2906d8737fe8436306ba07333ae1
snipzer <l.arif@laposte.net>
2017-03-21 09:28:22 +0100

    Ajout de la gestion des évènements dans le constructeur

--- 05-Person/js/Person.js
+++ 05-Person/js/Person.js
@@ -1,7 +1,7 @@
 function Person(name, elemId){
     this.name = name;
     this.element = document.querySelector('#'+elemId);
-    this.element.querySelector(".name").innerText = this.name;
+    this.bindEvent();
 }
 
 Person.prototype.sayHi = function ()
@@ -14,4 +14,9 @@ Person.prototype.sayBye = function ()
     this.element.querySelector(".message").innerText = "Au revoir !";
 };
 
-
+Person.prototype.bindEvent = function ()
+{
+    this.element.querySelector(".name").innerText = this.name;
+    this.element.querySelector(".sayHi").onclick = Person.prototype.sayHi.bind(this);
+    this.element.querySelector(".sayBye").onclick = Person.prototype.sayBye.bind(this);
+};
