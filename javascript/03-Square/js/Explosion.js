d7cdbaa4829be381061a60dda1b915e9c43fcd92
snipzer <l.arif@laposte.net>
2017-03-22 16:22:20 +0100

    Correction du bug de spawn des explosions et changement de leurs couleur

--- javascript/03-Square/js/Explosion.js
+++ javascript/03-Square/js/Explosion.js
@@ -10,13 +10,13 @@ Explosion.prototype.createExplosion = function ()
     var explosion = document.createElement("div");
     var field = document.querySelector(".conteneur");
 
+    console.log(explosion.style.top);
+
     explosion.className = "explosion";
-    explosion.style.top = Math.floor((Math.random() * 800));
-    explosion.style.left = Math.floor((Math.random() * 800));
-    explosion.style.right = 800 - explosion.style.left;
-    explosion.style.bottom = 800 - explosion.style.bottom;
-    explosion.style.width = Math.floor((Math.random() * 100) + 50);
-    explosion.style.height = Math.floor((Math.random() * 100) + 50);
+    explosion.style.top = Math.floor((Math.random() * 800))+"px";
+    explosion.style.left = Math.floor((Math.random() * 800))+"px";
+    explosion.style.width = (Math.floor((Math.random() * 100) + 50))+"px";
+    explosion.style.height = (Math.floor((Math.random() * 100) + 50))+"px";
 
     field.appendChild(explosion);
 };
