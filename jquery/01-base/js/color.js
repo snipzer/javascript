7f10541e335d63472357da9e196224b8b624e6cc
snipzer <l.arif@laposte.net>
2017-03-22 15:13:14 +0100

    ajout des commentaires

--- jquery/01-base/js/color.js
+++ jquery/01-base/js/color.js
@@ -1,21 +1,24 @@
 $(document).ready(function()
 {
+    // La div id nom récupère la valeur de l'input type text
     $("input").keyup(function ()
     {
-        console.log("a");
         $("#nom").text($('input[type="text"]').val());
     });
 
+    // Le bouton rouge applique la couleur rouge
     $("#rouge").click(function()
     {
         $("#nom").css("color", 'red');
     });
 
+    // Le bouton bleu applique la couleur bleu
     $("#bleu").click(function()
     {
         $("#nom").css("color", 'blue');
     });
 
+    // Le bouton vert applique la couleur vert
     $("#vert").click(function()
     {
         $("#nom").css("color", 'green');
